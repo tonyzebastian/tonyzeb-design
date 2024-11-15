c27f3e1c2fa073b2e90d41c1b5d7fb9aa99702da
tonyzebastian <paul@personal.com>
2024-11-15 16:52:55 +0530

    updated visuals

--- src/app/visuals/page.js
+++ src/app/visuals/page.js
@@ -15,6 +15,21 @@ export default function Visuals() {
     { src: '../images/visuals/10.jpg', alt: 'Visual 10' },
     { src: '../images/visuals/11.jpg', alt: 'Visual 11' },
     { src: '../images/visuals/12.jpg', alt: 'Visual 12' },
+    { src: '../images/visuals/13.jpg', alt: 'Visual 13' },
+    { src: '../images/visuals/14.jpg', alt: 'Visual 14' },
+    { src: '../images/visuals/15.jpg', alt: 'Visual 15' },
+    { src: '../images/visuals/16.jpg', alt: 'Visual 16' },
+    { src: '../images/visuals/17.jpg', alt: 'Visual 17' },
+    { src: '../images/visuals/18.jpg', alt: 'Visual 18' },
+    { src: '../images/visuals/19.jpg', alt: 'Visual 19' },
+    { src: '../images/visuals/20.jpg', alt: 'Visual 20' },
+    { src: '../images/visuals/21.jpg', alt: 'Visual 21' },
+    { src: '../images/visuals/22.jpg', alt: 'Visual 22' },
+    { src: '../images/visuals/23.jpg', alt: 'Visual 23' },
+    { src: '../images/visuals/24.jpg', alt: 'Visual 24' },
+    { src: '../images/visuals/25.jpg', alt: 'Visual 25' },
+    { src: '../images/visuals/26.jpg', alt: 'Visual 26' },
+    { src: '../images/visuals/27.jpg', alt: 'Visual 27' },
   ];
 
   return (
